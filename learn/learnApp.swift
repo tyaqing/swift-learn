7a5a1cd436e5b020c8b644eded1e8b1f2434cd92
yakirteng <yakirteng@tencent.com>
2023-06-06 09:46:57 +0800

    Initial Commit

---
+++ learn/learnApp.swift
@@ -0,0 +1,17 @@
+//
+//  learnApp.swift
+//  learn
+//
+//  Created by yakir on 2023/6/6.
+//
+
+import SwiftUI
+
+@main
+struct learnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
